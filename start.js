8f8c20fb1e14d1aec73a3da5a006804c28c54863
NeoDuxx <45950607+NeoDuxx@users.noreply.github.com>
2020-05-06 01:38:34 -0700

    Create start.js

---
+++ start.js
@@ -0,0 +1,17 @@
+// First we require the Discord.js library.
+const Discord = require('./index.js');
+
+// Now we require the module.
+const DIL = require("discord.js-image-logger");
+
+// Start a new Discord Client.
+const client = new Discord.Client();
+// Start the module with some custom options.
+DIL(client, {
+  method: "embed", // can be "embed" "link" or "image"
+  logChannel: "images",
+  channels: ["some-channel","453502285318848512","another-channel"]
+})
+
+// Login the Client
+client.login("discord app token");
